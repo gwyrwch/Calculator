8080d7387ac8ab2220bc95141ddddea550c4e5ff
gwyrwch <gwyrwch@gmail.com>
2019-10-15 08:44:24 +0300

    runtime error when changing orientation in full version fixed

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Application;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements OnExpressionPass {
     private TextView mainTextView;
     private Calculator calculator;
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +26,46 @@ public class MainActivity extends AppCompatActivity implements OnExpressionPass 
 
         calculator = new Calculator();
 
-        int width = getWindowManager().getDefaultDisplay().getWidth() / 7;
-        int height = getWindowManager().getDefaultDisplay().getHeight() / 4;
+        // todo: make if with current activity orientation
+        // todo: when changing orientation Activity recreates
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            //fixme: refactor this code and method setButtonMode
 
-        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
-        height = (int) ((height/displayMetrics.density)+0.5);
-        width = (int) ((width/displayMetrics.density)+0.5);
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-        System.out.println("kek");
-        System.out.println(width);
-        System.out.println(height);
+            Basic fragment = new Basic();
+            fragmentTransaction.add(R.id.fragment_frame, fragment);
+            fragmentTransaction.commit();
+            currentFragment = fragment;
+            setButtonMode();
+
+        }
 
 
+    }
+
+    private void setButtonMode() {
+        final Button btn = findViewById(R.id.button_mode);
+        btn.setOnClickListener(v -> {
+            Fragment newFragment;
+            int id;
+            if (currentFragment.getClass() == Basic.class) {
+                System.out.println("mda");
+                newFragment = new ScientificModeFragment();
+                id = R.id.fragment_frame;
+            } else {
+                System.out.println("lol");
+                newFragment = new Basic();
+                id = R.id.fragment_frame;
+            }
+            currentFragment = newFragment;
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.add(id, newFragment);
+            transaction.addToBackStack(null);
+
+            transaction.commit();
+        });
     }
 
     @Override
@@ -47,5 +82,13 @@ public class MainActivity extends AppCompatActivity implements OnExpressionPass 
     @Override
     public Calculator getCalculator() {
         return calculator;
+    }
+
+    private Fragment getCurrentFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        String fragmentTag = fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount() - 1).getName();
+
+        return getSupportFragmentManager()
+                .findFragmentByTag(fragmentTag);
     }
 }
